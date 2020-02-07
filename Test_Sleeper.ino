7cfa41c2d3b587360e6444b936adf919f62f51a8
Aidan Fredericks <45866370+KidMalicious@users.noreply.github.com>
2020-02-07 12:12:50 -0700

    Add files via upload
    These are the main files for the operations of the LoRa Node and the Dragino Gateway.
    LoRA Node: Test_Sleeper, SerialPassThrough, LoRa_Client_Gateway, mod_client.
    Gateway: Lora_Server.

---
+++ Test_Sleeper.ino
@@ -0,0 +1,16 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial1.begin(115200);
+  while (!Serial && !Serial1) {
+  } // Wait for serial port to be available
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+bool Sleeper(){
+  
+  }
